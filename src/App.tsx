b20e14cb241f9442a716b266926f9425dbf87354
Scarlet <scarryaa@gmail.com>
2024-03-12 20:24:18 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Login from './screens/Login';
 
 function App() {
+  const handleLogin = (username: string, password: string) => {
+    console.log(`Username: ${username}, Password: ${password}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Login onSubmit={handleLogin} />
     </div>
   );
 }
---
+++ src/screens/Login.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import './Login.scss';
+
+interface LoginProps {
+  onSubmit: (username: string, password: string) => void;
+}
+
+const Login: React.FC<LoginProps> = ({ onSubmit }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSubmit(username, password)
+  };
+
+  return (
+    <div className='login-container'>
+        <div className='login-logo'>
+            <span>yumesky</span>
+        </div>
+        <form onSubmit={handleSubmit} className='login-form'>
+            <label htmlFor="username">Username:</label>
+            <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <label htmlFor="password">Password:</label>
+            <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Login</button>
+        </form>
+    </div>
+  );
+};
+
+export default Login;
